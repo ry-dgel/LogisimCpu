c0025572a236e60a0bc5b472c192809f9a08f88a
ry-dgel <rydgel.code@gmail.com>
2017-03-02 16:14:04 -0500

    Added assembly and binary multiplication program

---
+++ mult.asm
@@ -0,0 +1,16 @@
+0 LD  R0, D
+1 LD  R1, F
+2 ADD R1, R0
+3 STR R1, F
+4 LD  R0, C
+5 LD  R1, E
+6 ADD R1, R0
+7 STR R1, E
+8 INP R0
+9 BNQ R0, R1, 0
+A LD  RO, F
+B PNT R0
+C STP
+D #7_10  //Number to mult: a
+E #0_10  //Counter
+F #0_10  //Result
